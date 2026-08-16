a4bb2c321cc7b6fe2529a9f581fe47dffb22fac4
digianparfum-droid <SEU_EMAIL_DO_GITHUB>
2026-08-15 22:34:54 -0300

    Remove scripts de sondagem deixados pelos agentes (arquivos temporarios de investigacao, nao fazem parte do site)

--- scripts/_probe-ts.mjs
+++
@@ -1,4 +0,0 @@
-import { PILLARS } from "../lib/pillars.ts";
-import { CLARO_PILLAR } from "../components/claro/claroPillarAccent.ts";
-console.log(PILLARS.map(p => p.key + "=" + p.label).join(" | "));
-console.log("claroPillar keys:", Object.keys(CLARO_PILLAR ?? {}));
